a0c015c133320342913b628c2ca15e3c8d6fdb83
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-02-19 12:02:54 +0300

    enable cors

--- gateway/src/main.ts
+++ gateway/src/main.ts
@@ -4,6 +4,7 @@ import { AnyExceptionFilter } from "./filters/any-exception.filter";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new AnyExceptionFilter());
   await app.listen(3000);
 }
